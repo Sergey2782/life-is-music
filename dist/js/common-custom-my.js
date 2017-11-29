011485f3d938772948ada649c881dc9ddf25d12a
Sergey2782 <msergun234@gmail.com>
2017-11-29 03:00:30 +0300

    Added style for elementsIcon.png(shadow), added - BTN-TO-TOP

--- dist/js/common-custom-my.js
+++ dist/js/common-custom-my.js
@@ -1,21 +1,45 @@
+var btnClickMail_1 = document.querySelector('.link_Post_1'),
+    btnClickMail_2 = document.querySelector('.link_Post_2'),
+    btnClickMail_3 = document.querySelector('.link_Post_3');
 
-  var btnClickMail_1 = document.querySelector('.link_Post_1'),
-      btnClickMail_2 = document.querySelector('.link_Post_2'),
-      btnClickMail_3 = document.querySelector('.link_Post_3');
-  
-      btnClickMail_1.onclick = hideMailopenMailwin;
-      btnClickMail_2.onclick = hideMailopenMailwin;
-      btnClickMail_3.onclick = hideMailopenMailwin;
+    btnClickMail_1.onclick = hideMailopenMailwin;
+    btnClickMail_2.onclick = hideMailopenMailwin;
+    btnClickMail_3.onclick = hideMailopenMailwin;
 
-     var m = new Array('moc.liamg@432nugresm');
+var m = new Array('moc.liamg@432nugresm');
 
-     function hideMailopenMailwin (){
-          var w = window.open();
-          w.window.location="mailto:" + m[0].split('').reverse().join('');
-  
- }
-/*
-$(".cont").css('width',function(i,value){
-    return parseFloat(value)*1.2;
-})
-*/
+    function hideMailopenMailwin (){
+       var w = window.open();
+       w.window.location="mailto:" + m[0].split('').reverse().join('');
+    }
+
+var $btnTop = $('.btn-totop');
+
+$(window).on('scroll', function(){
+     if($(window).scrollTop() >= 200){
+         $btnTop.fadeIn();
+     }else{
+         $btnTop.fadeOut();
+     }
+  });
+
+$btnTop.on('click', function(){
+     $('html,body').animate({scrollTop:0}, 900)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
